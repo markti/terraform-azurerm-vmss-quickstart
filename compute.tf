7394e43f42efed763741c31afc0b732775c7c586
mtinderholt <mtinderholt@microsoft.com>
2022-12-13 15:34:40 -0500

    three zone names

--- compute.tf
+++ compute.tf
@@ -5,13 +5,13 @@ resource "tls_private_key" "ssh" {
 
 module "vmz1" {
 
-  count = 0
+  count = var.vm_count
 
   source = "./modules/vm"
 
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  name                = "vm${local.suffix}${count.index}"
+  name                = "vm${local.suffix}z1${count.index}"
   vm_size             = var.vm_size
   subnet_id           = azurerm_subnet.main.id
   ssh_key             = tls_private_key.ssh.public_key_openssh
@@ -27,10 +27,26 @@ module "vmz2" {
 
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  name                = "vm${local.suffix}${count.index}"
+  name                = "vm${local.suffix}z2${count.index}"
   vm_size             = var.vm_size
   subnet_id           = azurerm_subnet.main.id
   ssh_key             = tls_private_key.ssh.public_key_openssh
   zone                = 2
+
+}
+
+module "vmz3" {
+
+  count = var.vm_count
+
+  source = "./modules/vm"
+
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  name                = "vm${local.suffix}z3${count.index}"
+  vm_size             = var.vm_size
+  subnet_id           = azurerm_subnet.main.id
+  ssh_key             = tls_private_key.ssh.public_key_openssh
+  zone                = 3
 
 }
